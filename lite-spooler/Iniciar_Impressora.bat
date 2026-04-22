57b0d7dff7aebb0014f54049a8cee0603eeb558b
Leo System <leo@churrascaria.com>
2026-04-22 18:33:16 -0300

    Refactor: Spooler ultra-simplified for Windows compatibility

--- lite-spooler/Iniciar_Impressora.bat
+++ lite-spooler/Iniciar_Impressora.bat
@@ -1,17 +1,3 @@
-@echo off
-title Léo Churrascaria - Spooler de Impressao
-color 0A
-echo ==================================================
-echo   BEM VINDO AO CLIENTE DE IMPRESSAO DA CHURRASCARIA 
-echo ==================================================
-echo.
-echo.
-echo Verificando dependencias...
-if not exist node_modules (
-    echo Instalando modulos necessarios (apenas na primeira vez)...
-    call npm install
-)
-
+cd %~dp0
 node app-impressora.js
 pause
-
